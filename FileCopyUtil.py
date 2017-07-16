a85f8ea1de56ae87bdd9ea2215313df54fa10ed9
Clinton Wang <clintonwang@gmail.com>
2017-07-15 21:49:15 -0400

    full gui, error handling

--- FileCopyUtil.py
+++ FileCopyUtil.py
@@ -15,85 +15,62 @@ import xlrd
 import os
 import easygui
 import numpy as np
-import csv
+from pandas import DataFrame
 import codecs
 
-easygui.msgbox('This utility allows you to search a directory to retrieve subfolders and filenames that contain MRNs. It produces a .csv with all matches and copies \
-    the files/folders to a separate directory.')
 
-#get the excel sheet
-#print('Choose patient data sheet')
+def find_name_in_list(name, ls):
+    matches = []
+    for item_name in ls:
+        if name in item_name:
+            matches.append(item_name)
+
+    return matches
+
+
+# UI flow
+if not easygui.msgbox('This utility allows you to search a directory to retrieve subfolders and filenames that contain MRNs. It produces a .csv with all matches and copies \
+the files/folders to a separate directory.'):
+    exit(0)
+
 master_ws = easygui.fileopenbox(msg='Choose patient data sheet', filetypes=["*.xlsx", "*.xls"])
-workbook = xlrd.open_workbook(master_ws)
-ws = workbook.sheet_by_index(0)
-#num_rows = ws.nrows
+if master_ws is None:
+    exit(0)
 
-#get correct MRN column
 col = easygui.integerbox(msg='Enter the column with patient MRNs (A=0, B=1, etc): ')
-#col = int(input ('Enter the column with patient MRNs (A=0, B=1, etc): '))
+if col is None:
+    exit(0)
 
-#file browser for choosing which directory to index
-#print ('Choose directory for indexing')
-path = easygui.diropenbox(msg='Choose directory to search')
-filename = easygui.enterbox(msg='Name the output file:', default='results.csv')
-copy_dir = easygui.enterbox(msg='Name the new directory to receive copied files:', default='results')
+path = easygui.diropenbox(msg='Select a folder to search.')
+if path is None:
+    exit(0)
+
+try:
+    exc_dirs = easygui.enterbox(msg="Enter the name of any subfolders to exclude (case-sensitive). Separate by commas, \
+    do not include slashes, and do not specify the path. e.g. animal, rabbit images, Alice's folder").split(', ')
+except:
+    exit(0)
+
+
+# Default parameters. Can be converted to UI options if necessary.
 show_progress = True
+filename = 'FileCopyDirectory.csv'#easygui.enterbox(msg='Name the output file.', default='results.csv')
+copy_dir = 'FileCopyResults'#easygui.enterbox(msg='Name the new directory to receive copied files (do not include slashes).', default='results')
 
-exc_dirs = easygui.enterbox(msg="Enter the name of any subfolders to exclude (case-sensitive). Separate by commas, \
-    do not include slashes, and do not specify the path. e.g. animal, rabbit images, Aaron's stuff").split(', ')
+
+# Preprocess user inputs
 if len(exc_dirs)==1 and exc_dirs[0]=='':
     exc_dirs=[]
 
-#choose output csv
-#print ('Name the output file (.csv)')
-#filename = easygui.filesavebox(msg='Name the output file (e.g. results.csv)')
-#copy_dir = input('Name the new directory to receive copied files:')
-#show_progress = input('Choose whether to display the progress of the file search as Y or N:')
-#show_progress = easygui.ynbox(msg='Display the progress of the file search?')
-
-#sorts directory titles into array of strings
-#folders = [f for f in sorted(os.listdir(path))]
-#folders = np.asarray(folders, dtype=str)
-
-#gets worksheet row values and puts into an array of strings
+# Get list of MRNs to search
+ws = xlrd.open_workbook(master_ws).sheet_by_index(0)
 try:
-    arr = [int(ws.cell(i,col).value) for i in range(ws.nrows)]
+    patient_ids = [int(ws.cell(i,col).value) for i in range(ws.nrows)]
 except:
     easygui.msgbox("Parsing error. May be due to wrong column selected or non-numeric entry present. This program will now exit.")
-    exit()
+    exit(1)
 
-print(arr)
-#for i in range(1,num_rows+1):
-#     row = np.asarray(ws.row_values(i))
-#     arr = np.append(arr, [row], axis = 0)
 
-"""arr2 = arr[:,1]
-mrn_str = str(int(float(str(arr2[1]))))
-for x in range (2, len(arr2)):
-     mrn_str = mrn_str + "," + str(int(float(str(arr2[x]))))
-"""
-
-'''
-#The subsequent section of code should only be activated if the script is being run on a Mac
-
-argument_parser = argparse.ArgumentParser()
-argument_parser.add_argument('-d', '--directory', type=str, required=True, help='The directory to search recursively.')
-argument_parser.add_argument('-p', '--patient_ids', type=str, required=True, help='Patient IDs to look for comma-separated without whitepsace (e.g. 123,852,489)')
-argument_parser.add_argument('-o', '--output_file', type=str, help='The name of the output file where results should be saved (tab-delimited format).')
-argument_parser.add_argument('-v', '--verbose', type=str, help='Print out progress information about directory structure.')
-argument_parser.add_argument('-c', '--copy_files', type=str, help='Copy files to this new directory')
-
-arguments = argument_parser.parse_args()
-
-path = arguments.directory
-mrn_str = arguments.patient_ids
-copy_dir = arguments.copy_files
-show_progress = arguments.verbose
-filename = arguments.output_file
-
-'''
-
-counter = 0
 #creates dictionary
 paths_by_patient_id = dict((patient_id, set()) for patient_id in mrn_str.split(','))
 
@@ -103,10 +80,17 @@ for root, subdirs, files in os.walk(path):
         if exc_dir in subdirs:
             subdirs.remove(exc_dir)
 
+    for patient_id in patient_ids:
+        matching_dirs = find_name_in_list(patient_id, subdirs)
+
+        if patient_id in subdirs:
+            subdirs.remove(exc_dir)
+
     for subdir in subdirs:
+        counter = 0
         matched_patients = []
 
-        for patient_id in paths_by_patient_id:
+        for patient_id in patient_ids:
 
             if patient_id in subdir:
                 paths_by_patient_id[patient_id].add(root + '/' + subdir)
@@ -124,10 +108,8 @@ for root, subdirs, files in os.walk(path):
                 print(raw.encode('utf-8', errors='replace'))
 
 
-
     #creates counter and empty set for matched patients
     for file in files:
-
         matched_patients = []
 
         for patient_id in paths_by_patient_id:
@@ -161,11 +143,8 @@ if filename != None:
 
 #copy matched patients to new directory
 for patient_id in paths_by_patient_id:
-
     raw = patient_id
-    
     print (raw.encode('utf-8', errors='replace'))
-
     my_set = paths_by_patient_id[patient_id]
 
     for set_contents in my_set:
@@ -180,11 +159,3 @@ for patient_id in paths_by_patient_id:
                     
             else:
                 copytree(set_contents, new_path)
-
-          
-
-                
-
-        
-
-
