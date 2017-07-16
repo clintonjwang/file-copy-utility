5f9cc3c68481084cd9cfe18011f1c0a8900e7cc0
Clinton Wang <clintonwang@gmail.com>
2017-07-16 18:24:45 -0400

    handle duplicate filenames

--- FileCopyUtil.py
+++ FileCopyUtil.py
@@ -13,10 +13,8 @@ from shutil import copytree, copyfile
 import argparse
 import xlrd
 import os
+from csv import writer
 import easygui
-import numpy as np
-#from pandas import DataFrame
-import codecs
 
 # return all members of name_list that contain name
 def find_name_in_list(name, name_list):
@@ -27,35 +25,38 @@ def find_name_in_list(name, name_list):
 
     return matches
 
-
 # UI flow
-def setup_ui():
-    if not easygui.msgbox('This utility allows you to search a directory to retrieve subfolders and filenames that contain MRNs. \
-It produces a .csv with all matches and copies the files/folders to a separate directory. MRNs must be stored in an .xlsx or .xls format. \
-\nWARNING: This assumes MRNs do not begin with 0 and have a fixed number of digits.'):
+def setup_ui(skip_col=True, skip_exc=True):
+    if not easygui.msgbox(('This utility searches a directory to retrieve subfolders and filenames that contain MRNs. '
+                        'It will copy these files/folders to a separate directory. MRNs must be stored in an .xlsx or .xls format. '
+                        'WARNING: This assumes MRNs do not begin with 0 and have a fixed number of digits.')):
         exit(0)
 
     mrn_src = easygui.fileopenbox(msg='Choose patient data sheet.', filetypes=["*.xlsx", "*.xls"])
     if mrn_src is None:
         exit(0)
 
-    col = easygui.integerbox(msg='Enter the column with patient MRNs (A=0, B=1, etc): ')
-    if col is None:
-        exit(0)
+    if skip_col:
+        col = 0
+    else:
+        col = easygui.integerbox(msg='Enter the column with patient MRNs (A=0, B=1, etc): ')
+        if col is None:
+            exit(0)
 
     search_path = easygui.diropenbox(msg='Select a folder to search.')
     if search_path is None:
         exit(0)
 
-    try:
-        exc_dirs = easygui.enterbox(msg="Enter the name of any subfolders to exclude (case-sensitive). Leave blank to include all folders. Separate by commas, \
-do not include slashes, and do not specify the path. e.g. animal, rabbit images, Alice's folder.").split(', ')
-
-        if len(exc_dirs) == 1 and exc_dirs[0] == '':
-            exc_dirs = []
-
-    except:
-        exit(0)
+    if skip_exc:
+        exc_dirs = []
+    else:
+        try:
+            exc_dirs = easygui.enterbox(msg=("Enter the name of any subfolders to exclude (case-sensitive). Leave blank to include all folders. Separate by commas,"
+                                            "do not include slashes, and do not specify the path. e.g. animal, rabbit images, Alice's folder.")).split(', ')
+            if len(exc_dirs) == 1 and exc_dirs[0] == '':
+                exc_dirs = []
+        except:
+            exit(0)
 
     # Get list of MRNs to search
     ws = xlrd.open_workbook(mrn_src).sheet_by_index(0)
@@ -68,71 +69,108 @@ do not include slashes, and do not specify the path. e.g. animal, rabbit images,
 
     return patient_ids, search_path, exc_dirs
 
+# Get matching files and directories for each MRN
+def get_matching_paths(patient_ids, search_path, exc_dirs, show_progress=True):
+    # dict to store matching paths
+    paths_by_patient_id = dict((patient_id, []) for patient_id in patient_ids)
 
+    # to track progress
+    match_dir_cnt = 0
+    match_file_cnt = 0
+    dir_cnt = 0
 
-# Default parameters. Can be converted to UI options if necessary.
-show_progress = True
-output_csv = 'FileCopyDirectory.csv'#easygui.enterbox(msg='Name the output file.', default='results.csv')
-copy_dir = 'FileCopyResults'#easygui.enterbox(msg='Name the new directory to receive copied files (do not include slashes).', default='results')
+    #search for matching folders/files
+    for root, subdirs, files in os.walk(search_path):
 
-# Ask user for inputs
-patient_ids, search_path, exc_dirs = setup_ui()
+        # exclude directories specified by user
+        for exc_dir in exc_dirs:
+            if exc_dir in subdirs:
+                subdirs.remove(exc_dir)
 
-# dict to store matching paths
-paths_by_patient_id = dict((patient_id, []) for patient_id in patient_ids)
+        for patient_id in patient_ids:
+            matching_dirs = find_name_in_list(patient_id, subdirs)
+            matching_files = find_name_in_list(patient_id, files)
 
-# to track progress
-match_counter = 0
-dir_counter = 0
+            for matching_dir in matching_dirs:
+                paths_by_patient_id[patient_id].append(root + '/' + matching_dir)
+                subdirs.remove(matching_dir)
+                match_dir_cnt += 1
 
-#search for matching folders/files
-for root, subdirs, files in os.walk(search_path):
+            for matching_file in matching_files:
+                paths_by_patient_id[patient_id].append(root + '/' + matching_file)
+                match_file_cnt += 1
 
-    # exclude directories specified by user
-    for exc_dir in exc_dirs:
-        if exc_dir in subdirs:
-            subdirs.remove(exc_dir)
+        dir_cnt += 1
+        if show_progress and dir_cnt % 10 == 1:
+            print(dir_cnt, " directories explored, ", match_file_cnt, " matching files found, and ", match_dir_cnt,
+                " matching folders found. (Last directory explored: ", root, ")", sep="")
 
-    for patient_id in patient_ids:
-        matching_dirs = find_name_in_list(patient_id, subdirs)
-        matching_files = find_name_in_list(patient_id, files)
+    print("Search complete. ", dir_cnt, " directories explored and ", match_file_cnt,
+        " matching files found, and ", match_dir_cnt, " matching folders found.", sep="")
 
-        for matching_dir in matching_dirs:
-            paths_by_patient_id[patient_id].append(root + '/' + matching_dir)
-            subdirs.remove(matching_dir)
-            match_counter += 1
+    return paths_by_patient_id
 
-        for matching_file in matching_files:
-            paths_by_patient_id[patient_id].append(root + '/' + matching_file)
-            match_counter += 1
+# write MRNs and matching paths to a csv
+def write_to_csv(paths_by_patient_id, output_csv, pause_before_copy=False):
+    with open(output_csv, 'w') as f:
+        csv_writer = writer(f)
+        for patient_id in paths_by_patient_id:
+            csv_writer.writerow([patient_id] + paths_by_patient_id[patient_id])
 
-    if show_progress:
-        dir_counter += 1
-        print(dir_counter, " directories explored and ", match_counter, " total matches found. (Last directory explored: ", root, ")", sep="")
+    if pause_before_copy:
+        if not easygui.ynbox("Matches written to " + output_csv + ". Copy matching files to a new directory?"):
+            exit(0)
+    else:
+        print("Matches written to ", output_csv, ". Starting to copy matching files.", sep="")
 
-exit(0)
-#navigating through dictionary through folders
-with codecs.open(output_csv, 'w', 'utf-8') as f:
+# Write matching files to new directory
+def copy_matching_files(paths_by_patient_id, copy_dir, show_progress=True):
+    potential_duplicates = []
+
     for patient_id in paths_by_patient_id:
-        raw_write = '%s\t%s\n' % (patient_id, '\t'.join(sorted(paths_by_patient_id[patient_id])))
-        f.write(raw_write)
+        base_dir = os.getcwd() + '/' + copy_dir + '/' + patient_id
+        try:
+            os.mkdir(base_dir)
+        except:
+            pass
 
+        for match in paths_by_patient_id[patient_id]:
+            new_path = base_dir + '/' + os.path.basename(match)
 
-#copy matched patients to new directory
-for patient_id in paths_by_patient_id:
-    raw = patient_id
-    print(raw.encode('utf-8', errors='replace'))
-    my_set = paths_by_patient_id[patient_id]
-
-    for set_contents in my_set:
-
-        if patient_id in os.path.basename(set_contents) and set_contents.count(patient_id) == 1:
-            new_path = copy_dir + '/' + os.path.basename(set_contents)
-            raw = set_contents
-            print(raw.encode('utf-8', errors='replace'))
-
-            if '.' in os.path.basename(set_contents):
-                copyfile(set_contents, new_path)
-                    
+            if '.' in os.path.basename(match):
+                try:
+                    copyfile(match, new_path)
+                except:
+                    potential_duplicates.append(os.path.basename(match))
             else:
-                copytree(set_contents, new_path)
+                try:
+                    copytree(match, new_path)
+                except:
+                    potential_duplicates.append(os.path.basename(match) + '/')
+
+    if len(potential_duplicates) > 0:
+        easygui.msgbox('Copy complete. Potential file duplicates detected. Only the first one found was copied. See duplicates.log file.')
+        with open('duplicates.log', 'w') as f:
+            f.write('\n'.join(potential_duplicates))
+    else:
+        easygui.msgbox('Copy complete. Please report any issues or requests at https://github.com/clintonjwang/glowing-octo-potato/issues')
+
+def main():
+    # Default parameters. Can be converted to UI options if necessary.
+    output_csv = 'FileCopyDirectory.csv'
+    copy_dir = 'FileCopyResults'
+
+    # Ask user for inputs
+    patient_ids, search_path, exc_dirs = setup_ui()
+
+    # Get matching files and directories for each MRN
+    paths_by_patient_id = get_matching_paths(patient_ids, search_path, exc_dirs)
+
+    # Write matches to csv
+    write_to_csv(paths_by_patient_id, output_csv)
+
+    # Write matching files to new directory
+    copy_matching_files(paths_by_patient_id, copy_dir)
+
+if __name__ == "__main__":
+    main()
